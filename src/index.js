52e6fe558f8ff22b97a68588036caf6701aaf29a
Nastassia Khamitsevich <hronofaqia@gmail.com>
2022-08-06 01:32:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n >= 0) {
+  const strReverse = n.toString().split('').reverse().join('');
+  return +strReverse;
+    } else {
+        const strMinReverse = n.toString().split('').slice(1).reverse().join('');
+  return +strMinReverse;
+    }
 }
